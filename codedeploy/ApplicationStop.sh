483e28398ce12599c823deac2155d3595d1a4b33
MUHAMMAD JAHANZEB <mjhanzaibmemon123@gmail.com>
2024-02-29 20:36:12 +0500

    load secrets in .env before application start

--- codedeploy/ApplicationStop.sh
+++ codedeploy/ApplicationStop.sh
@@ -1,6 +1,8 @@
-if /home/ec2-user/.nvm/versions/node/v20.11.1/bin/pm2 list | grep -q "online"; then
+export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.11.1/bin/
+
+if pm2 list | grep -q "online"; then
     echo "Deleting all pm2 processes."
-    /home/ec2-user/.nvm/versions/node/v20.11.1/bin/pm2 delete all
+    pm2 delete all
 else
     echo "No processes found to stop."
 fi

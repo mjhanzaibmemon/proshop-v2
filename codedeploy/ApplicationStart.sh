056efd519e178c591caa0d1aba7869acd510513f
MUHAMMAD JAHANZEB <mjhanzaibmemon123@gmail.com>
2024-03-01 08:10:35 +0500

    secret manager id renamed

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -1,4 +1,9 @@
-export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.11.1/bin/
+# load secrets before start
+SECRET_JSON=$(aws secretsmanager get-secret-value --secret-id Proshop --query SecretString --output text)
+echo "$SECRET_JSON" | jq -r "to_entries|map(\"\(.key | ascii_upcase)=\(.value | tostring)\")|.[]" > /Proshop/.env
 
+
+# start server
+export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.11.1/bin/
 cd /Proshop
 pm2 start npm -- start

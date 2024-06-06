bf0fc45cac51ee55606184372b886046164aca55
MUHAMMAD JAHANZEB <53127947+mjhanzaibmemon@users.noreply.github.com>
2024-06-06 21:41:58 +0000

    Update ApplicationStart.sh

--- codedeploy/ApplicationStart.sh
+++ codedeploy/ApplicationStart.sh
@@ -4,6 +4,6 @@ echo "$SECRET_JSON" | jq -r "to_entries|map(\"\(.key | ascii_upcase)=\(.value | 
 
 
 # start server
-export PATH=$PATH:/home/ec2-user/.nvm/versions/node/v20.11.1/bin/
+export PATH=$PATH:/home/jhezi/.nvm/versions/node/v20.11.1/bin/  
 cd /Proshop
 pm2 start npm -- start
